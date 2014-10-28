67c8572d05450bfa26100dc6932589815003139c
Pablo Molina <p2kmgcl@gmail.com>
2014-10-28 13:04:36 +0100

    Añadido pseudocodigo

--- practica2/practica2/GRASP.cpp
+++ practica2/practica2/GRASP.cpp
@@ -1,4 +1,23 @@
 #include "GRASP.h"
 
+int* generarSolucionGreedyAleatorizada () {
+}
+
+int* busquedaLocal (int* solucionInicial) {
+}
+
+int* seleccionarMejorSolucion (int* solucion1, int* solucion2) {
+}
+
 GRASP::GRASP() {
+	bool criterioParada;
+	int* solucionActual;
+	int* mejorSolucionVecina;
+	int* mejorSolucion;
+
+	while (criterioParada) {
+		solucionActual = generarSolucionGreedyAleatorizada();
+		mejorSolucionVecina = busquedaLocal(solucionActual);
+		mejorSolucion = seleccionarMejorSolucion(mejorSolucionVecina, mejorSolucion);
+	}
 }
--- practica2/practica2/ILS.cpp
+++ practica2/practica2/ILS.cpp
@@ -1,4 +1,37 @@
 #include "ILS.h"
 
+int* generarSolucionInicial () {
+}
+
+int* busquedaLocal (int* solucionInicial) {
+}
+
+int* modificar (int* solucionActual, void* historia) {
+}
+
+int* criterioAceptacion (int* solucionActual, int* solucionVecinaMejorada, void* historia) {
+}
+
+int* seleccionarMejorSolucion (int* solucionActual, int* mejorSolucion) {
+}
+
 ILS::ILS() {
+	bool criterioDeParada;
+	void* historia;
+
+	int* solucionActual;
+	int* solucionVecina;
+	int* solucionVecinaMejorada;
+	int* mejorSolucion;
+
+	solucionActual = generarSolucionInicial();
+	solucionActual = busquedaLocal(solucionActual);
+
+	do {
+		solucionVecina = modificar(solucionActual, historia);
+		solucionVecinaMejorada = busquedaLocal(solucionVecina);
+		solucionActual = criterioAceptacion(solucionActual, solucionVecinaMejorada, historia);
+		mejorSolucion = seleccionarMejorSolucion(solucionActual, mejorSolucion);
+
+	} while (criterioDeParada);
 }
