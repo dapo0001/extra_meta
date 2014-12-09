e93302c9b7ad4e05bfba0b2c9b140a950fae7fe8
Pablo Molina <p2kmgcl@gmail.com>
2014-12-09 10:51:24 +0100

    Integrado contador de generaciones y salida a consola en el algoritmo generacional

--- practica3/practica3/AlgoritmoGeneticoGeneracional.cpp
+++ practica3/practica3/AlgoritmoGeneticoGeneracional.cpp
@@ -3,34 +3,34 @@
 using namespace std;
 
 #include "Poblacion.h"
+#include "Solucion.h"
 
 #define DEBUG
 
 void AlgoritmoGeneticoGeneracional::ejecutar (QAP& qap, int semilla) {
 	this->iniciarTiempoEjecucion();
-	int generaciones = 0;
 
 	Poblacion* poblacionActual = 0;
 	Poblacion* poblacionVecina = 0;
 	Poblacion* nuevaPoblacion = 0;
 
 	poblacionActual = new Poblacion(&qap, qap.getNumComp());
-	cout << poblacionActual->getMejorIndividuo()->getValorSolucionActual() << endl;
+	this->valorSolucionInicial = poblacionActual->getMejorIndividuo()->getValorSolucionActual();
 
-	while (generaciones++ < 20000) {
+	Solucion::llamadasAFuncionObjetivo = 0;
+	while (Solucion::llamadasAFuncionObjetivo < 5000) {
 		poblacionVecina = poblacionActual->clonar();
 		poblacionVecina->seleccion();
 		//poblacionVecina->cruzar();
 		poblacionVecina->cruzarPMX();
 		poblacionVecina->mutar();
 		nuevaPoblacion = poblacionActual->combinar(poblacionVecina);
-		
-		cout<<"Generacion "<<generaciones<<endl;
 		poblacionActual = nuevaPoblacion->clonar();
 	}
 	valorSolucion = poblacionActual->getMejorIndividuo()->getValorSolucionActual();
-	cout << valorSolucion << endl;
+	
 	this->finalizarTiempoEjecucion();
+	this->mostrarResultados();
 
 	delete poblacionActual;
 }
